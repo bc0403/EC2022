1b91c3ae98252c1d426265e13d2f1a2c9c5c211d
bc0403 <hjin.zj@gmail.com>
2022-07-23 17:51:50 +0800

    build framework

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+## 0.1.0
+
+Initialiation and framework build
--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -6,7 +6,20 @@ export default {
   title: 'Fundamentals of Electronic Circuits',
   description: 'course material',
   base: '/EC2022',
+  lastUpdated: true,
+  //Theme related configurations
+  themeConfig: {
+    sidebar: {
+      '/': sidebarEC()
+      // '/mc/': sidebarMC()
+    },
 
+    socialLinks: [
+      {icon: 'github', link: 'https://github.com/bc0403/EC2022'}
+    ]
+  },
+
+  // for latex
   markdown: {
     config: (md) => {
       md.use(mathjax3);
@@ -20,3 +33,35 @@ export default {
     },
   },
 };
+
+function sidebarEC() {
+  return [
+    {
+      text: 'Electric Circuits',
+      collapsible: true,
+      items: [
+        { text: '课程简介', link: '/' },
+        { text: '电路基本概念', link: '/concepts' },
+        { text: '电路分析的基本方法和定理', link: '/methods' },
+        { text: '正弦交流电路', link: '/phasor' },
+        { text: '过渡过程的经典解法', link: '/transition' },
+        { text: '谐振电路和滤波器', link: '/resonator' },
+        { text: '磁耦合电路', link: '/coupling' }
+      ]
+    },
+    {
+      text: 'Microelectronic Circuits',
+      collapsible: true,
+      items: [
+        { text: '二极管', link: '/diode' },
+        { text: '三极管', link: '/bjt' },
+        { text: '场效应晶体管', link: '/mosfet' },
+        { text: '差分和多级放大器', link: '/diff' },
+        { text: '频率响应', link: '/freq' },
+        { text: '反馈', link: '/feedbak' },
+        { text: '功率放大器', link: '/pa' },
+        { text: '运算放大器', link: '/opam' }
+      ]
+    }
+  ]
+}
